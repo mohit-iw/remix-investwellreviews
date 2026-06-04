2db9661fd288d0172e24f1405683f34e9ae92c11
mohit-iw <mohit.sharma@investwellonline.com>
2026-06-04 12:37:44 +0530

    deleted

--- src/lib/testimonials.functions.ts
+++
@@ -1,8 +0,0 @@
-import { createServerFn } from "@tanstack/react-start";
-import { loadTestimonials } from "./testimonials.server";
-
-export const getTestimonials = createServerFn({ method: "GET" }).handler(
-  async () => {
-    return loadTestimonials();
-  },
-);
